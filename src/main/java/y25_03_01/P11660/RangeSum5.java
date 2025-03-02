d17f86f8e7f9884ce85a3018848fd43525d03ae4
hjlee5701 <hjlee5701@gmail.com>
2025-03-02 17:04:50 +0900

    [11660] 구간 합 구하기 5 수정

--- src/main/java/y25_03_01/P11660/RangeSum5.java
+++ src/main/java/y25_03_01/P11660/RangeSum5.java
@@ -19,20 +19,17 @@ public class RangeSum5 {
 
             for (int j = 1; j <= N; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                S[i][j] = S[i][j-1] + num;
+                S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + num;
             }
         }
 
         while (M-- > 0) {
-            int sumNum = 0;
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            for (int mx = x1; mx <= x2; mx++) {
-                sumNum += S[mx][y2] - S[mx][y1-1];
-            }
+            int sumNum = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1];
             System.out.println(sumNum);
         }
     }
